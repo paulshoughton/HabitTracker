75702528d77be83a895ad451557a34e75131af36
Paul Houghton <paul.houghton@gmail.com>
2020-10-04 08:32:36 +0100

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Paul Houghton on 04/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
